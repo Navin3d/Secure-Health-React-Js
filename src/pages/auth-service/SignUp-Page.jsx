3d761295b90803c324b5af52d05b4bad5c8e0c38
logasubramanism <smlogasubramani@gmail.com>
2023-01-26 16:12:15 +0530

    bookappointmentpage

--- src/pages/auth-service/SignUp-Page.jsx
+++ src/pages/auth-service/SignUp-Page.jsx
@@ -8,7 +8,7 @@ const SignUpPage = () => {
                     <div className="col-md-1"></div>
                     <div className="col-md-5">
                         <h5 class="head-content">Create An Account<br></br><p className='tagline'>At your Quick service </p></h5>
-                
+                    
                         <label for="fname" id="label">First name:</label><br></br><input type="text" id="fname" name="fname"/>
                         <br></br>
                         <label for="lname" id="label">Email:</label><br></br><input type="text" id="fname" name="lname"/>
@@ -29,7 +29,7 @@ const SignUpPage = () => {
                         </div>
                     </div>
                         <div className="col-md-5">
-                           <img className='image' src="https://img.freepik.com/free-photo/doctor-with-stethoscope-hands-hospital-background_1423-1.jpg?w=1060&t=st=1674720399~exp=1674720999~hmac=0727a68a2687d7e6e9c71d6610d850d417f6768af1a97ddbc6ca4fcfc31ccc3e" alt="" />
+                           <img className='image' src="https://img.freepik.com/free-photo/fun-3d-cartoon-illustration-indian-doctor_183364-114483.jpg?w=360&t=st=1674722971~exp=1674723571~hmac=d69cc26f40533bd60fbf22d99df7a92fd85189a4421d3237f0784cfe759f1420" alt="" />
                         </div>
                 </div>
             </div>
--- src/pages/patient-service/BookAppoinmentPage.jsx
+++ src/pages/patient-service/BookAppoinmentPage.jsx
@@ -1,7 +1,33 @@
+import '../../styles/pages/Bookappointment.css'
+// import book from '../../assets/images/bookappointment.png'
 const BookAppoinment = () => {
     return (
         <div>
-            <h1>BookAppoinment Page</h1>
+            <div className="content-title">
+                <h2>Make an Appointment</h2>
+                <p>---------------------</p>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2" ></div>
+                    <div className="col-md-8" id="box">
+                        <div className="row">
+                            <div className="col">
+                            <input type="text" placeholder='Summary'id="fname1"/>
+                            <input type="text" placeholder='Description'id="fname1"/>
+                            <input type="date" placeholder='Appointment time'id="fname1"/>
+                            <input type="date" placeholder='Appointment date'id="fname1"/>
+                            <button className='bookbtn'>Book Now</button>
+                            </div>
+                            <div className="col">
+                            <img className='image1' src="https://img.freepik.com/free-vector/doctor-examining-patient-clinic-illustrated_23-2148856559.jpg?w=1060&t=st=1674728590~exp=1674729190~hmac=da4935b4d2d07247f036e51709501a145e63a4aab93c5595b92f0249c7f4c34d" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                  
+                    <div className="col-md-2" ></div>
+                </div>
+            </div>
         </div>
     );
 }
