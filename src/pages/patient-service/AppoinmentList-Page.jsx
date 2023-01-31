ac0376b51b5c6fac752fd5314589cfea3f2b7f00
logasubramanism <smlogasubramani@gmail.com>
2023-01-31 22:47:06 +0530

    app_list structure

--- src/pages/patient-service/AppoinmentList-Page.jsx
+++ src/pages/patient-service/AppoinmentList-Page.jsx
@@ -1,18 +1,36 @@
+// import App_Img from '../../assets/images/profile.jpg';
+import '../../styles/pages/Appointmentlist.css'
 const AppoinmentList = () => {
     return (
         <div>
             <div className="content-title">
-            <h2>Appointment listing page</h2>
+            <h2>You'r Appointments</h2>
             <p>---------------------</p>
             </div>
+
+{/* Pending Appoinments */}
             <div className="container">
-                <div className="row">
-                    <div className="box-list">
+                <div className="pen_app">
+                    <p>Pending Appointments</p>
+                    <p>------------------</p>
+                </div>
+                <div className="row">    
+                <table>
+                    <tr>
+                       <th>Appointment_type</th>
+                       <th>Due date</th>
+                       <th>Due Time</th>
+                       <th>Location</th>
+                       <th>Doctor name</th>
+                    </tr>
+                    <tr>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                    </tr>
 
-
-                    <div className="col"></div>
-
-                    </div>
+                 </table>
                 </div>
             </div>
         </div>
