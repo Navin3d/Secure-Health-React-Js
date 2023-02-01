abaf0e55abe3297bc8cc96290de89a52eb572913
logasubramanism <smlogasubramani@gmail.com>
2023-02-01 07:12:42 +0530

    structure update

--- src/pages/patient-service/AppoinmentList-Page.jsx
+++ src/pages/patient-service/AppoinmentList-Page.jsx
@@ -4,8 +4,7 @@ const AppoinmentList = () => {
     return (
         <div>
             <div className="content-title">
-            <h2>You'r Appointments</h2>
-            <p>---------------------</p>
+                
             </div>
 
 {/* Pending Appoinments */}
@@ -24,10 +23,40 @@ const AppoinmentList = () => {
                        <th>Doctor name</th>
                     </tr>
                     <tr>
-                       <td></td>
-                       <td></td>
-                       <td></td>
-                       <td></td>
+                       <td>Viral Fever</td>
+                       <td>12-05-23</td>
+                       <td>09:00</td>
+                       <td>udhayar block</td>
+                       <td>Dr.j.priya</td>
+                    </tr>
+
+                 </table>
+                </div>
+            </div>
+
+{/* previous appoinments */}
+        <div className="content-title"></div>
+
+            <div className="container">
+                <div className="pen_app">
+                    <p>Previous Appoinments</p>
+                    <p>------------------</p>
+                </div>
+                <div className="row">    
+                <table>
+                    <tr>
+                       <th>Appointment_type</th>
+                       <th>Due date</th>
+                       <th>Due Time</th>
+                       <th>Location</th>
+                       <th>Doctor name</th>
+                    </tr>
+                    <tr>
+                       <td>Viral Fever</td>
+                       <td>12-05-23</td>
+                       <td>09:00</td>
+                       <td>udhayar block</td>
+                       <td>Dr.j.priya</td>
                     </tr>
 
                  </table>
