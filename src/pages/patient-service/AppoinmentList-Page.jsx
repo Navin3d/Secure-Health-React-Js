2f2685dc9f282a3256293f619bb2d09ca534b2ab
logasubramanism <smlogasubramani@gmail.com>
2023-02-02 00:18:57 +0530

    appointment list page responsive over

--- src/pages/patient-service/AppoinmentList-Page.jsx
+++ src/pages/patient-service/AppoinmentList-Page.jsx
@@ -3,33 +3,57 @@ import '../../styles/pages/Appointmentlist.css'
 const AppoinmentList = () => {
     return (
         <div>
-            <div className="content-title">
-                
+            <div className="app_list_title">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h6>
+                            Appoinments
+                        </h6>
+                    </div>
+                    <div className="col-md-3" id='media_move'>
+                        <label className='fil_lab' htmlFor="">Filter</label>
+                        <input className='Filter_box' type="text" placeholder='search' />
+                    </div>
+                    <div id='media_move' className="col-md-3">
+                        <div class="dropdown">
+                          <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            SortBy
+                          </button>
+                          <div  class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Date</a>
+                            <a class="dropdown-item" href="#">A-Z</a>
+                            <a class="dropdown-item" href="#">Z-A</a>
+                          </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
+            <hr />
 {/* Pending Appoinments */}
             <div className="container">
                 <div className="pen_app">
                     <p>Pending Appointments</p>
-                    <p>------------------</p>
                 </div>
                 <div className="row">    
                 <table>
-                    <tr>
+                    <tr className='table_row_head' > 
+                        <th>#</th>
                        <th>Appointment_type</th>
                        <th>Due date</th>
                        <th>Due Time</th>
                        <th>Location</th>
                        <th>Doctor name</th>
+                       <th>Status</th>
                     </tr>
                     <tr>
+                        <td>1</td>
                        <td>Viral Fever</td>
                        <td>12-05-23</td>
                        <td>09:00</td>
                        <td>udhayar block</td>
                        <td>Dr.j.priya</td>
+                       <td>confirmed</td>
                     </tr>
-
                  </table>
                 </div>
             </div>
@@ -40,11 +64,11 @@ const AppoinmentList = () => {
             <div className="container">
                 <div className="pen_app">
                     <p>Previous Appoinments</p>
-                    <p>------------------</p>
                 </div>
                 <div className="row">    
                 <table>
-                    <tr>
+                <tr className='table_row_head' > 
+                        <th>#</th>
                        <th>Appointment_type</th>
                        <th>Due date</th>
                        <th>Due Time</th>
@@ -52,6 +76,7 @@ const AppoinmentList = () => {
                        <th>Doctor name</th>
                     </tr>
                     <tr>
+                        <td>1</td>
                        <td>Viral Fever</td>
                        <td>12-05-23</td>
                        <td>09:00</td>
